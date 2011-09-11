966dc64f084e4a6a70d3c4910dbee447c1456fae
oscloudsdp <oscloudsdp@gmail.com>
2011-09-11 04:52:45 +0000

    init

---
+++ infra-be/src/main/java/com/oscloudsdp/infra/App.java
@@ -0,0 +1,13 @@
+package com.oscloudsdp.infra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
